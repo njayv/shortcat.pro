6a92155b85735402939c3119005df36180855370
Niall <niallvassallo097@gmail.com>
2024-06-19 00:59:22 +0100

    Broken no more!!! And added a few commands to test out how it works. Also added a few images to work with the json file when trying to call a map.

--- Cogs/TestCmd.py
+++ Cogs/TestCmd.py
@@ -1,7 +1,7 @@
 from discord import Option, ApplicationContext, slash_command
 from discord.ext import commands
 
-class TestCmd(commands.Cog):
+class testcmd(commands.Cog):
 
     def init(self, bot):
         self.bot: commands.Bot = bot
@@ -10,5 +10,14 @@ class TestCmd(commands.Cog):
     async def Test(self, ctx: commands.Context):
         await ctx.send("It didn't work :clueless:")
 
+    @slash_command(name='ping')
+    async def ping(self, ctx: ApplicationContext):
+        await ctx.respond("Pong!")
+    
+    @slash_command(name='choose')
+    async def choose(self, ctx: ApplicationContext, selection=Option(name='uhh', description='Pick this one!', choices=['A', 'B', 'C'])):
+        await ctx.respond(f"You selected: {selection}")
+
+
 def setup(bot: commands.Bot):
-    bot.add_cog(TestCmd(bot))
+    bot.add_cog(testcmd(bot))
