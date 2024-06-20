a4db0d30c03ee2c689444c21ccee55abb4e16cfb
Niall <niallvassallo097@gmail.com>
2024-06-20 15:07:10 +0100

    Created test command for sending image. Added all base game track icons.

--- Cogs/TestCmd.py
+++ Cogs/TestCmd.py
@@ -1,3 +1,4 @@
+import discord
 from discord import Option, ApplicationContext, slash_command
 from discord.ext import commands
 
@@ -14,9 +15,13 @@ class testcmd(commands.Cog):
     async def ping(self, ctx: ApplicationContext):
         await ctx.respond("Pong!")
     
-    @slash_command(name='choose', description='Choose a track to view info about')
+    @slash_command(name='choose', description='Choose a track to view info about.')
     async def choose(self, ctx: ApplicationContext, selection=Option(name='track', description='Name of track. E.g. MKS (Mario Kart Stadium)', choices=['MKS', 'Other', 'Not Sure'])):
         await ctx.respond(f"You selected: {selection}")
+    
+    @slash_command(name='map_image', description='Send and image of the track selected.')
+    async def map_image(self, ctx: ApplicationContext, map_selection=Option(name='track_name', description='Name of the track.', choices=['MKS', 'Not Implemented Yet'])):
+        await ctx.respond(file= discord.File(f'./map_images/{map_selection}.webp'))
 
 
 def setup(bot: commands.Bot):
