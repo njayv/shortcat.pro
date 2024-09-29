f2c179c8e0117bdfd5fcb6625950c35c6b77f56c
Niall <niallvassallo097@gmail.com>
2024-09-29 15:35:48 +0100

    - Removed "map_data.json" and using different form of data - Added "bulletspots", "strategy" and "tracks" jsons to hold certain information - removed cup_icons and replaced with cup_locations - added cup images to assist in locating a track - polished commands - polished embeds - added information output when using /track_strategy

--- Cogs/TestCmd.py
+++ Cogs/TestCmd.py
@@ -1,4 +1,5 @@
 import discord
+import json
 from discord import Option, ApplicationContext, slash_command
 from discord.ext import commands
 
@@ -9,27 +10,37 @@ class testcmd(commands.Cog):
 
     @slash_command(name='track_strategy', description="Display the Track Strategy of a given track name.")
     async def track_strat(self, ctx: ApplicationContext, map_selection=Option(name='track_name', description='Use Abbreviation Of Track!')):
+
+        with open("./track_jsons/strategy.json", "r+") as f:
+            strategies= json.load(f)
+            picked_strategy= [entry for entry in strategies if entry["code"] == map_selection]
+            if len(picked_strategy) == 0:
+                return await ctx.respond("That's not a track! Try using the abbriviation of the track you'd like to see.\n-# For example: MKS = Mario Kart Stadium, rMMM = Moo Moo Meadows ('r' meaning Retro Cup), dBP = Baby Park ('d' meaning DLC), bKC ('b' meaning Booster Course)")
+            picked_strategy= picked_strategy[0]           
+        
         embed_msg = discord.Embed(
             colour= discord.Color.random(),
             title= str(map_selection),
-            
+            thumbnail= f"https://raw.githubusercontent.com/njayv/shortcat.pro/master/map_images/{str(map_selection).lower()}.webp",
+            description= picked_strategy["description"]
             )
-        
-        embed_msg.set_footer(text= "Images and descriptions taken from the 'shortcat.pro' website.", icon_url= 'https://shortcat.pro')
-        embed_msg.set_image(url= f"https://raw.githubusercontent.com/njayv/shortcat.pro/master/map_images/{map_selection.lower()}.webp")
+
+        #embed_msg.set_footer(text= "Images and descriptions taken from the 'shortcat.pro' website.", icon_url= 'https://shortcat.pro')
+        #embed_msg.set_image(url= f"https://raw.githubusercontent.com/njayv/shortcat.pro/master/map_images/{str(map_selection).lower()}.webp")
 
         await ctx.respond(embed= embed_msg)
 
     @slash_command(name='item_map', description="Display the Item Map of a given track name.")
     async def item_map(self, ctx: ApplicationContext, map_selection=Option(name='track_name', description='Use Abbreviation Of Track!')):
+
         embed_msg = discord.Embed(
             colour= discord.Color.random(),
             title= str(map_selection),
-            
+            description= "Test"
             )
         
         embed_msg.set_footer(text= "Images and descriptions taken from the 'shortcat.pro' website.", icon_url= 'https://shortcat.pro')
-        embed_msg.set_image(url= f"https://raw.githubusercontent.com/njayv/shortcat.pro/master/map_images/item_dist_map/{map_selection.lower()}.webp")
+        embed_msg.set_image(url= f"https://raw.githubusercontent.com/njayv/shortcat.pro/master/map_images/item_dist_map/{str(map_selection).lower()}.webp")
 
         await ctx.respond(embed= embed_msg)
 
